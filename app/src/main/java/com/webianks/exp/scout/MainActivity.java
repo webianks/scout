e3e6627c7a371f34a1755df89fedb423423ef216
webianks <webianks@gmail.com>
2017-02-20 01:45:05 +0530

    Getting AlchemyLanguage started

--- app/src/main/java/com/webianks/exp/scout/MainActivity.java
+++ app/src/main/java/com/webianks/exp/scout/MainActivity.java
@@ -3,11 +3,17 @@ package com.webianks.exp.scout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyLanguage;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlchemyLanguage service = new AlchemyLanguage();
+        service.setApiKey(BuildConfig.API_KEY);
+
     }
 }
