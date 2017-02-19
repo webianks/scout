308765d20ccc11882b7d7439b00a6cab34e44f1d
webianks <webianks@gmail.com>
2017-02-20 00:53:07 +0530

    added input_data

--- src/scout/Scout.java
+++ src/scout/Scout.java
@@ -5,10 +5,27 @@
  */
 package scout;
 
+import edu.stanford.nlp.coref.CorefCoreAnnotations.CorefChainAnnotation;
+import edu.stanford.nlp.coref.data.CorefChain;
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
+import edu.stanford.nlp.util.CoreMap;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
 
 /**
  *
@@ -21,7 +38,7 @@ public class Scout {
      * @throws java.io.FileNotFoundException
      */
     public static void main(String[] args) throws FileNotFoundException, IOException {
-       
+        
         FileReader fileReader = new FileReader("C:\\Users\\R Ankit\\Desktop\\Appathon\\Input.txt");
         BufferedReader br = new BufferedReader(fileReader);
         
@@ -32,21 +49,54 @@ public class Scout {
             
               numberOfLines++;
               Scout scout = new Scout();
-              scout.tokenizeTheLine(singleLine);
+              scout.processLine(singleLine);
               System.out.println();
             
           }
            br.close();
            //System.out.println("Number of lines "+numberOfLines);
            
+       
     }
-
-    private void tokenizeTheLine(String singleLine) {
-        for(String s : singleLine.split(" "))
-           processWord(s);
+    
+    private void processLine(String line) {
+        
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+          
+        Annotation document = new Annotation(line);
+        pipeline.annotate(document);
+        
+        // these are all the sentences in this document
+        // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
+       List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+       
+       for(CoreMap sentence: sentences) {
+       // traversing the words in the current sentence
+       // a CoreLabel is a CoreMap with additional token-specific methods
+         for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+          // this is the text of the token
+          String word = token.get(TextAnnotation.class);
+          // this is the POS tag of the token
+          String pos = token.get(PartOfSpeechAnnotation.class);
+         // this is the NER label of the token
+          String ne = token.get(NamedEntityTagAnnotation.class);
+               
+        }
+    
+     // this is the parse tree of the current sentence
+     Tree tree = sentence.get(TreeAnnotation.class);  
+     // this is the Stanford dependency graph of the current sentence
+     SemanticGraph dependencies = sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);  
+     
     }
-
-    private void processWord(String s) {
-        System.out.println(s);
-    }
+      
+       // This is the coreference link graph
+      // Each chain stores a set of mentions that link to each other,
+     // along with a method for getting the most representative mention
+    // Both sentence and token offsets start at 1!
+     Map<Integer, CorefChain> graph = document.get(CorefChainAnnotation.class); 
+     
+   }
 }
