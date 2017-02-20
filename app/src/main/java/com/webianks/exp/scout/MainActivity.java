3455c9f026bc983fd421657ebbbed04ec105e0c8
webianks <webianks@gmail.com>
2017-02-21 00:12:21 +0530

    Getting file from MATERIAL_PICKER_PICKER

--- app/src/main/java/com/webianks/exp/scout/MainActivity.java
+++ app/src/main/java/com/webianks/exp/scout/MainActivity.java
@@ -1,20 +1,29 @@
 package com.webianks.exp.scout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 import com.webianks.exp.scout.model.NamedEntities;
 import com.webianks.exp.scout.model.TypedRelations;
 import com.webianks.exp.scout.network.ApiServices;
 import com.webianks.exp.scout.network.RestClient;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
 
@@ -23,10 +32,16 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //testCalls();
+
+    }
+
+    private void testCalls() {
+
         ApiServices apiService = new RestClient().getApiService();
 
-        Call<NamedEntities> namedEntitiesCall = apiService.getNamedEntities("PDFs from Ravi in the last 3 days","json");
-        Call<TypedRelations> typedRelationsCall = apiService.getTypedRelations("PDFs from Ravi in the last 3 days","json");
+        Call<NamedEntities> namedEntitiesCall = apiService.getNamedEntities("PDFs from Ravi in the last 3 days", "json");
+        Call<TypedRelations> typedRelationsCall = apiService.getTypedRelations("PDFs from Ravi in the last 3 days", "json");
 
         //asynchronous call
         namedEntitiesCall.enqueue(new Callback<NamedEntities>() {
@@ -35,8 +50,8 @@ public class MainActivity extends AppCompatActivity{
 
                 if (response.isSuccessful()) {
                     NamedEntities namedEntities = response.body();
-                    Toast.makeText(MainActivity.this,"Got the entities",Toast.LENGTH_LONG).show();
-                    Log.d(TAG,namedEntities.getEntities().get(0).getText());
+                    Toast.makeText(MainActivity.this, "Got the entities", Toast.LENGTH_LONG).show();
+                    Log.d(TAG, namedEntities.getEntities().get(0).getText());
                 }
             }
 
@@ -52,8 +67,8 @@ public class MainActivity extends AppCompatActivity{
                 if (response.isSuccessful()) {
 
                     TypedRelations typedRelations = response.body();
-                    Toast.makeText(MainActivity.this,"Got the relations",Toast.LENGTH_LONG).show();
-                    Log.d(TAG,typedRelations.getTypedRelations().get(0).getType());
+                    Toast.makeText(MainActivity.this, "Got the relations", Toast.LENGTH_LONG).show();
+                    Log.d(TAG, typedRelations.getTypedRelations().get(0).getType());
 
                 }
             }
@@ -63,6 +78,30 @@ public class MainActivity extends AppCompatActivity{
 
             }
         });
-
     }
+
+    public void showFileChooser(View view) {
+
+        new MaterialFilePicker()
+                .withActivity(this)
+                .withRequestCode(1)
+                .withFilter(Pattern.compile(".*"))
+                .withFilterDirectories(true)
+                .withHiddenFiles(true)
+                .start();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+
+            String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+
+        }
+    }
+
+
 }
